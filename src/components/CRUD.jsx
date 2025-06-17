c7f97dc4857b09508b5461404072c7ec76f2bb3f
Diego Martín <diegomtnfranco@gmail.com>
2025-06-17 20:41:55 -0300

    Commit: 0.0.1

---
+++ src/components/CRUD.jsx
@@ -0,0 +1,13 @@
+import Balance from "./Balance";
+import TablaCRUD from "./TablaCRUD"
+
+const CRUD = () => {
+    return (
+        <div>
+            <Balance />
+            <TablaCRUD />
+        </div>
+    )
+}
+
+export default CRUD;
